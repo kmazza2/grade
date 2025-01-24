fe81e459565ea704e0033136ae245f132a6712ba
Kevin Mazzarella <kmazza2@uic.edu>
2025-01-24 09:23:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+The file `grade.py` is a zero-dependency CSV editor **with tab completion** aimed at helping TAs quickly record grades. (Windows users may need Cygwin or WSL2. If you are having trouble, let me know.) You download a CSV of student grades from Blackboard, run the script, give it the path to the CSV file when prompted, choose if you want to assign grades by **name** or by **ID number**, and choose which assignment to grade. It then prompts you for names or ID numbers (which can be entered only partially then tab completed) and the corresponding grades. It is most useful for grading paper assignments, which are often received out of alphabetical order.
+
+```
+$ cat test.csv
+First,Last,UIN,First Quiz,Second Quiz
+John,Boyle,38291,,9
+Bob,Palermo,91823,0,
+Lucy,Burke,11928,,
+John,Santora,83921,3,9
+$ python3 grade.py 
+Path to grades csv: 
+grade.py  README.md test.csv 
+Path to grades csv: te<TAB>
+test.csv
+
+Assign grades by
+1) Name
+2) UIN
+(type the number without quotes, then press Return): 1
+
+Which column corresponds to FIRST NAME?
+1) First
+2) First Quiz
+(type the number without quotes, then press Return): 1
+
+Which column corresponds to LAST NAME?
+1) Last
+(type the number without quotes, then press Return): 1
+
+Type the name of the assessment exactly as it appears on Blackboard (case sensitive).
+> <TAB><TAB>
+First       First Quiz  Last        Second Quiz UIN        
+> Se<TAB>
+> Second Quiz
+
+Begin grading.
+Type QUIT at the prompt to stop grading and write file.
+
+Student: Lu<TAB>
+Lucy Burke
+Grade: 98
+
+Student: John<TAB><TAB> 
+John Boyle   John Santora
+Student: John S<TAB>
+John Santora
+Grade: 92
+
+Student: Bob<TAB>
+Bob Palermo
+Grade: 98
+
+Student: John Boyle
+Grade: 84
+
+Student: QUIT
+
+Successfully wrote grades to updated_grades.csv.
+
+$ cat updated_grades.csv 
+First,Last,UIN,First Quiz,Second Quiz
+John,Boyle,38291,,84
+Bob,Palermo,91823,0,98
+Lucy,Burke,11928,,98
+John,Santora,83921,3,92
+```
---
+++ grade.py
@@ -0,0 +1,191 @@
+import csv
+import os
+import readline
+import sys
+
+
+GREEN = '\x1b[32m'
+RESET = '\x1b[0m'
+
+
+class Completer(object):
+
+    def __init__(self, options):
+        self.options = sorted(options)
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            if text:
+                self.matches = [
+                    option
+                    for option in self.options
+                    if option and option.startswith(text)
+                ]
+            else:
+                self.matches = self.options
+        if state < len(self.matches):
+            response = self.matches[state]
+        else:
+            response = None
+        return response
+
+
+if sys.platform == 'darwin':
+    readline.parse_and_bind('bind ^I rl_complete')
+else:
+    readline.parse_and_bind('tab: complete')
+readline.set_completer(None)
+readline.set_completer_delims('\n')
+
+path = input(GREEN + 'Path to grades csv: ' + RESET).strip()
+while not os.path.isfile(path):
+    print('File does not exist.')
+    print()
+    path = input(GREEN + 'Path to grades csv: ' + RESET).strip()
+with open(path, mode='r', newline='') as original_csv:
+    data = [record for record in csv.DictReader(original_csv)]
+    if not data:
+        raise Exception('No data.')
+original_columns = [column for column in data[0]]
+columns = sorted(original_columns)
+
+readline.set_completer(
+    Completer(('1', '2')).complete
+)
+print()
+print('Assign grades by\n' +
+'1) Name\n' +
+'2) UIN'
+)
+choice = None
+while choice not in ('1', '2'):
+    choice = input(GREEN + '(type the number without quotes, then press Return): ' + RESET)
+if choice == '1':
+    key = 'name'
+elif choice == '2':
+    key = 'uin'
+else:
+    raise Exception('Impossible state.')
+
+index = dict()
+if key == 'name':
+    first_columns = [column for column in columns if 'first' in column.lower()]
+    last_columns = [column for column in columns if 'last' in column.lower()]
+    readline.set_completer(
+        Completer([str(i + 1) for i in range(len(first_columns))]).complete
+    )
+    print()
+    print('Which column corresponds to FIRST NAME?')
+    for (i, column_name) in enumerate(first_columns, start=1):
+        print(f'{i}) {column_name}')
+    column_index = None
+    while column_index not in list(range(1, len(first_columns) + 1)):
+        column_index = int(
+            input(
+                GREEN +
+                '(type the number without quotes, then press Return): ' +
+                RESET
+            )
+        )
+    first_name_column = first_columns[column_index - 1]
+    readline.set_completer(
+        Completer([str(i + 1) for i in range(len(last_columns))]).complete
+    )
+    print()
+    print('Which column corresponds to LAST NAME?')
+    for (i, column_name) in enumerate(last_columns, start=1):
+        print(f'{i}) {column_name}')
+    column_index = None
+    while column_index not in list(range(1, len(last_columns) + 1)):
+        column_index = int(
+            input(
+                GREEN +
+                '(type the number without quotes, then press Return): ' +
+                RESET
+            )
+        )
+    last_name_column = last_columns[column_index - 1]
+    for record in data:
+        first_name = record[first_name_column]
+        last_name = record[last_name_column]
+        name = first_name + ' ' + last_name
+        if name in index:
+            raise Exception('Two students have the same name. Retry with UINs instead. Aborting.')
+        index[name] = record
+elif key == 'uin':
+    uin_columns = [column for column in columns if 'id' in column.lower() or 'uin' in column.lower()]
+    readline.set_completer(
+        Completer([str(i + 1) for i in range(len(uin_columns))]).complete
+    )
+    print()
+    print('Which column corresponds to UIN?')
+    for (i, column_name) in enumerate(uin_columns, start=1):
+        print(f'{i}) {column_name}')
+    column_index = None
+    while column_index not in list(range(1, len(uin_columns) + 1)):
+        column_index = int(
+            input(
+                GREEN +
+                '(type the number without quotes, then press Return): ' +
+                RESET
+            )
+        )
+    uin_column = uin_columns[column_index - 1]
+    for record in data:
+        uin = record[uin_column]
+        if uin in index:
+            raise Exception('Two students have the same UIN. Aborting.')
+        index[uin] = record
+else:
+    raise Exception('Impossible state.')
+keys = [key for key in index]
+
+readline.set_completer(
+    Completer(columns).complete
+)
+print()
+print(
+    GREEN +
+    'Type the name of the assessment exactly as it appears on Blackboard (case sensitive).' +
+    RESET
+)
+assessment_column = None
+while assessment_column not in columns:
+    assessment_column = input('> ')
+
+readline.set_completer(
+    Completer(keys).complete
+)
+print()
+print('Begin grading.\nType QUIT at the prompt to stop grading and write file.')
+key_prompt = (
+    GREEN + 
+    (
+        'Student: '
+        if key == 'name'
+        else 'UIN: '
+    ) +
+    RESET
+)
+while True:
+    print()
+    current_key = None
+    while current_key != 'QUIT' and current_key not in index:
+        current_key = input(key_prompt)
+    if current_key == 'QUIT':
+        break
+    grade = input(GREEN + 'Grade: ' + RESET)
+    if grade == 'QUIT':
+        break
+    index[current_key][assessment_column] = grade
+
+# Should check if this file already exists...
+with open('updated_grades.csv', mode='w', newline='') as new_csv:
+    csv_writer = csv.DictWriter(new_csv, original_columns)
+    csv_writer.writeheader()
+    for key in index:
+        csv_writer.writerow(index[key])
+print()
+print('Successfully wrote grades to updated_grades.csv.')
+print()
